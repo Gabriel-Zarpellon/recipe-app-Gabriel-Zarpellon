772b43223615b9f49fb7d1c2da37e0ecd5f6f002
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-08-02 16:48:56 -0300

    feat: add recipes initial database

---
+++ src/data/index.js
@@ -0,0 +1,52 @@
+export const recipeList = [
+  {
+    img: "https://img.delicious.com.au/i9g6ECfY/w759-h506-cfill/del/2022/05/gnocchi-168125-1.png",
+    name: "Creamy harissa gnocchi",
+    level: "advanced",
+    meal: "supper",
+    servings: "4",
+    ingredients: [
+      "1kg fresh store-bought gnocchi",
+      "40g butter",
+      "100g cream cheese",
+      "2 tbs rose harissa",
+      "A large handful of finely chopped chives",
+      "Grated parmesan, to serve",
+    ],
+    method:
+      "Cook the gnocchi in a large pan of boiling water for 2-3 minutes, or as directed on the packet, until they float to the surface and are lovely and fluffy. Using a heatproof jug, carefully scoop out 100ml of the cooking water for the sauce and then drain the gnocchi well. Melt the butter in a large non-stick frypan over a medium heat. Depending on the size of your pan, add gnocchi in batches. You don't want too much or it won't fry, so make sure there a little space around each piece. Add a pinch of salt and fry for 4-5 minutes until golden on one side. Repeat.Give the gnocchi in the pan a good shake and add the cream cheese, the remaining gnocchi, rose harissa and the reserved water. Mix well and cook for 1-2 minutes until the sauce clings to the gnocchi. Add most of the chives and mix well. Check the seasoning and add salt to taste. Divide the gnocchi between four serving bowls and top with the remaining chives and a grating of parmesan. Serve immediately.",
+  },
+  {
+    img: "https://img.delicious.com.au/oKB5Hw_e/w759-h506-cfill/del/2024/07/breakfast-tacos-215241-2.jpg",
+    name: "Breakfast tacos",
+    level: "intermediate",
+    meal: "breakfast",
+    servings: "4",
+    ingredients:
+      "4 tortillas (flour or corn, your choice). 4 crispy fried eggs, to serve. 3 sausages of your choice (fresh chorizo, pork & fennel, or just regular barbecue sausages, see note). 1 tbs vegetable oil. 16-20 frozen tater tots or potato gems, defrosted on paper towel. 2 small handfuls of rocket. 6 cherry tomatoes, halved or quartered. Hot sauce of your choice, to serve. Crumbled feta or Cotija (aged Mexican cheese, from specialty delis), to serve. Coriander sprigs, to serve. Lime wedges, to serve",
+    method:
+      " Preheat oven to 150°C/130°C fan-forced. Warm your tortillas. I like to flip mine over the flame of my gas burner to give them a little char. Then wrap them in foil and place in the oven to keep warm (you can skip the charring and just go straight to warming in foil in the oven if you like). Place fried eggs on a plate, cover loosely with foil and place in oven to keep warm. Use hands to squish the sausage meat out of their casings in chunks. Discard casings. Place a wok over medium-high heat. When it’s just starting to smoke, swirl in your oil and add your sausage chunks. Toss them around and use a spatula to break them up as much as you can. Cook for 3-4 minutes, until nice and charry at the edges and cooked through. Add tater tots and use a spatula (or potato masher) to squish into the sausage. The idea is to get some heat contact and crispiness on the flattened tots. Allow to cook for 3-4 minutes, until potato starts to crisp. Then start breaking up bits and flipping them over to get a good sear on the other side. Keep repeating until potato is golden and sausage a little charred. Place a tortilla on each plate. Scoop out chunks of sausage mixture onto the tortillas. Top with a little smattering of rocket and tomato, then place a crispy fried egg on each. Douse with as much hot sauce as you like. Sprinkle over the cheese and coriander, then serve with a lime wedge for squeezing over.",
+  },
+  {
+    img: "https://img.delicious.com.au/HI9V8DE4/w759-h506-cfill/del/2020/10/summer-tomato-and-tuna-pasta-with-pangrattato-140533-2.jpg",
+    name: "Tuna salad",
+    level: "begginer",
+    meal: "lunch",
+    servings: "4",
+    ingredients:
+      "350g pasta of your choice (we used fusilli).  100ml extra virgin olive oil, plus extra to drizzle. 3 garlic cloves, 2 crushed, 1 bruised. 4 slices sourdough bread, torn into large pieces. Finely grated zest of 2 lemons, plus juice of 1/2. 1/2 tsp dried chilli flakes, or to taste. 400g mixed cherry tomatoes, larger ones halved. 2 tsp red wine vinegar. 1 large can (425g) tuna, drained. 1 cup basil leaves, plus extra to serve.",
+    method:
+      "Preheat oven to 200°C. Bring a large saucepan of salted water to the boil. Add pasta and cook according to packet instructions. Drain, reserving 2 tbs cooking water, and return pasta to the pan. Drizzle with 1 tbs oil and toss to coat. Set aside, covered, and keep warm. To make the pangrattato, heat 2 tbs oil in a large frypan over medium heat. Add bruised garlic and breadcrumbs, and cook, stirring regularly, for 4 minutes. Add half the lemon zest and chilli, and season. Cook for a further 1-2 minutes until golden and crispy. Transfer to a tray lined with paper towel and set aside. Heat remaining 2 tbs oil in a large frypan over a high heat. Add tomatoes and cook, stirring occasionally, for 4 minutes or until tomatoes scorch and split. Break the tomatoes with the back of a spoon to release their juices. Add crushed garlic, remaining zest and chilli flakes, and cook for 1-2 minutes until tomatoes are tender and fragrant. Add lemon juice and vinegar, and cook for a further 1 minute, then add pasta and reserved cooking water. Cook for 1 minute or until warmed through, stirring regularly, then remove from the heat. Add tuna and basil, season and toss to combine. Scatter with pangrattato and extra basil leaves to serve.",
+  },
+  {
+    img: "https://img.delicious.com.au/tyb5bss6/w759-h506-cfill/del/2020/11/three-cheese-and-basil-pizza-141898-2.jpg",
+    name: "Cheese and basil pizza",
+    level: "begginer",
+    meal: "snack",
+    servings: "4",
+    ingredients:
+      "50g unsalted butter. 1 tsp fennel seeds.  1/3 cup (50g) plain flour. 200ml pure (thin) cream. 400ml milk. 60g finely grated parmesan. 1 cup (100g) shredded mozzarella. 200g Gorgonzola dolce, crumbled. Chilli flakes & basil leaves, to serve. Pizza dough:  1 1/2 tsp dried yeast. 1/2 tsp caster sugar. 5 cups (750g) tipo ‘00’ flour. 2 tbs extra virgin olive oil, plus extra to brush.",
+    method:
+      "Combine yeast, sugar and 450ml lukewarm water in a bowl, cover and stand for 10-15 minutes until frothy. Combine flour and 1 tbs salt flakes in a stand mixer fitted with the dough hook attachment. Add yeast mixture and oil, and knead for 10 minutes or until smooth and elastic. Transfer to a greased bowl, cover and stand in a warm place for 2 hours or until doubled in size. Divide dough into 4 balls and place on a tray. Cover and refrigerate for 24 hours. (Dough can be frozen for up to 6 months.) Stand at room temperature for 1 hour before baking. On a lightly floured surface, work with 1 piece of dough at a time and roll out into a rough 20cm x 40cm rectangle. Transfer to a tray and brush with extra olive oil. Repeat with remaining dough. Use with your preferred toppings. Preheat oven to 250°C. Melt butter and fennel seeds together in a small saucepan over low heat. Stir in flour. Gradually add cream and milk in 3 batches, stirring constantly to avoid lumps from forming. Add parmesan and mozzarella and stir until melted, then stir in half the Gorgonzola. Spoon cheese sauce over pizza bases and scatter over remaining 100g Gorgonzola. Bake for 14-16 minutes until golden. Scatter with chilli flakes and basil to serve.",
+  },
+];
