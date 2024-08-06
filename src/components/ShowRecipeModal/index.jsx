6ed5758b84a1d5951488295863353d8318512e90
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-08-06 16:34:15 -0300

    feat: add functions for edit modal and delete recipe

--- src/components/ShowRecipeModal/index.jsx
+++ src/components/ShowRecipeModal/index.jsx
@@ -3,34 +3,50 @@ import { StyledParagraph, StyledTitle } from "../../styles/typography";
 import { Modal } from "../Modal";
 import {
   StyledBottomRecipe,
+  StyledBox,
   StyledMidRecipe,
   StyledUpperRecipe,
 } from "./style";
+import {
+  closeRecipeThunk,
+  editRecipeModalThunk,
+} from "../../store/modules/modal/thunks";
+import { deleteRecipeThunk } from "../../store/modules/recipe/thunks";
 
 export function ShowRecipeModal() {
   const { openRecipe } = useSelector((state) => state);
 
   const currentRecipe = openRecipe.recipe;
 
+  const dispatch = useDispatch();
+
+  function handleDelete() {
+    dispatch(deleteRecipeThunk(currentRecipe.id));
+    dispatch(closeRecipeThunk());
+  }
   return (
     <Modal type="show" title="Recipe">
       <StyledUpperRecipe>
-        <div>
-          <StyledTitle number={1}>{currentRecipe.name}</StyledTitle>
+        <StyledTitle number={1}>{currentRecipe.name}</StyledTitle>
+        <StyledBox>
           <div>
             <StyledTitle number={2}>Ingredients</StyledTitle>
             <StyledParagraph>{currentRecipe.ingredients}</StyledParagraph>
           </div>
-        </div>
-        <img src={currentRecipe.img} alt={currentRecipe.name} />
+          <div>
+            <img src={currentRecipe.img} alt={currentRecipe.name} />
+          </div>
+        </StyledBox>
       </StyledUpperRecipe>
       <StyledMidRecipe>
         <StyledTitle>Method</StyledTitle>
         <StyledParagraph>{currentRecipe.method}</StyledParagraph>
       </StyledMidRecipe>
       <StyledBottomRecipe>
-        <button>Edit</button>
-        <button>Delete</button>
+        <button onClick={() => dispatch(editRecipeModalThunk(currentRecipe))}>
+          Edit
+        </button>
+        <button onClick={handleDelete}>Delete</button>
       </StyledBottomRecipe>
     </Modal>
   );
